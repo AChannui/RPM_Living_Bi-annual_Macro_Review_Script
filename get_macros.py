04ce333ed866fdc8462413ffcdbe7d52ff5ef028
Alexander Chan-Nui <alexander.channui@rpmliving.com>
2024-06-05 11:02:02 -0500

    changed id number formatting as well as hiding it from view

--- get_macros.py
+++ get_macros.py
@@ -13,6 +13,7 @@ import requests_cache
 
 from openpyxl import Workbook
 from openpyxl.styles import Font
+from openpyxl.descriptors import Integer
 
 def main():
     # argument parsing
@@ -46,6 +47,28 @@ def main():
 
     # creates dict of macros by group id
     grouped_macros = dict()
+    sort_macros(active_macros, grouped_macros)
+
+
+    # creates workbook with each group having their own sheet
+    wb = Workbook()
+    create_workbook(group_map, grouped_macros, wb)
+        
+
+
+
+    #print debugging
+    # print(wb.get_sheet_names())
+    del wb['Sheet']
+    # print debugging
+    # print(wb.get_sheet_names())
+    wb.save("macro_review.xlsx")
+
+    # print debugging
+    # print(len(macros))
+    print('done')
+
+def sort_macros(active_macros, grouped_macros):
     for macro in active_macros:
         resriction = macro["restriction"]
         # filtering out null and non Group restrictions
@@ -61,9 +84,7 @@ def main():
                 grouped_macros[id] = []
             grouped_macros[id].append(macro)
 
-
-    # creates workbook with each group having their own sheet
-    wb = Workbook()
+def create_workbook(group_map, grouped_macros, wb):
     for id, macro_list in grouped_macros.items():
         # sheet creation
         # print debugging
@@ -71,26 +92,24 @@ def main():
         ws1 = wb.create_sheet(group_map[id])
         # header
         ws1.append(['Name', "ID", "Created", "Updated", "Group", "Usage 30 Days", "Action", "Action Taken"])
+
+        #set font
         font = Font(bold=True, underline='single')
-        for col in range(1 ,ws1.max_column + 1):
-            ws1.cell(row=1, column=col).font = font
+        for cell in ws1['1:1']:
+            cell.font = font
 
         # fill sheet with macros from dict
         for macro in macro_list:
             macro_groups = ",".join (group_map[item] for item in macro['restriction']['ids'])
             ws1.append([macro["title"], macro["id"], macro["created_at"], macro["updated_at"], macro_groups, macro["usage_30d"]])
+        
+        # set id to show in regular form not scientific notation
+        colB = ws1['B']
+        for cell in colB:
+            cell.number_format = '0'
+        ws1.column_dimensions['B'].hidden=True
 
 
-    #print debugging
-    # print(wb.get_sheet_names())
-    del wb['Sheet']
-    # print debugging
-    # print(wb.get_sheet_names())
-    wb.save("macro_review.xlsx")
-
-    
-    pprint('done')
-    print(len(macros))
 
 # grabs all data from zendesk from url with key to specify what to grab
 def get_macro_list(session, next_url, key="macros"):
