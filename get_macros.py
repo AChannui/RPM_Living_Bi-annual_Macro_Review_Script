a4bf8ef932243d92d1962af244e48b32e7ee3032
Alexander Chan-Nui <alexander.channui@rpmliving.com>
2024-06-05 09:47:45 -0500

    added comments. also added deletion of first empty sheet. added bold and underlined header in sheets.

--- get_macros.py
+++ get_macros.py
@@ -12,60 +12,87 @@ import requests
 import requests_cache
 
 from openpyxl import Workbook
+from openpyxl.styles import Font
 
 def main():
+    # argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("--user", "-u", type=str, action="store", default="alexander.channui@rpmliving.com")
+    parser.add_argument("--user", "-u", type=str, action="store", default=os.getenv("ZENDESK_USERNAME"))
     parser.add_argument("--password", "-p", type=str, action="store", default=os.getenv("ZENDESK_PASSWORD"))
     args = parser.parse_args()
 
 
+    # request macros from zendesk
     # session = requests.Session()
     session = requests_cache.CachedSession('cache')
     session.auth = (args.user, args.password)
-    pprint(session.auth)
+    # print debugging
+    # print(session.auth)
     next_url = "https://roscoeproperties.zendesk.com/api/v2/macros.json?per_page=200&include=usage_30d"
     groups_url = "https://roscoeproperties.zendesk.com/api/v2/groups"
     
+    # getting all macros and narrowing down to all active macros
     macros = get_macro_list(session, next_url)
+    active_macros = [item for item in macros if item["active"]]
+    # print debugging
+    # print(len(active_macros))
 
+    # getting all group ids
     groups = get_macro_list(session, groups_url, "groups")
-    pprint(len(groups))
+    # print debugging
+    # print(len(groups))
+    # dict of group id and corresponding name
     group_map = {item["id"]:item["name"].replace("/", "") for item in groups if not item["deleted"]}
 
-    active_macros = [item for item in macros if item["active"]]
-    pprint(len(active_macros))
+    # creates dict of macros by group id
     grouped_macros = dict()
     for macro in active_macros:
         resriction = macro["restriction"]
+        # filtering out null and non Group restrictions
         if resriction is None:
             print(f"restriction is null, macro id number: {macro['id']}")
             continue
         if resriction["type"] != "Group":
             print(f"restriction not group, macro id number: {macro['id']}")
             continue
+        # creates new 
         for id in resriction['ids']:
             if id not in grouped_macros:
                 grouped_macros[id] = []
             grouped_macros[id].append(macro)
 
 
+    # creates workbook with each group having their own sheet
     wb = Workbook()
     for id, macro_list in grouped_macros.items():
-        print(group_map[id])
+        # sheet creation
+        # print debugging
+        # print(group_map[id])
         ws1 = wb.create_sheet(group_map[id])
-        ws1.append(["Name", "ID", "Created", "Updated", "Group", "Usage 30 Days", "Action", "Action Taken"])
+        # header
+        ws1.append(['Name', "ID", "Created", "Updated", "Group", "Usage 30 Days", "Action", "Action Taken"])
+        font = Font(bold=True, underline='single')
+        for col in range(1 ,ws1.max_column + 1):
+            ws1.cell(row=1, column=col).font = font
+
+        # fill sheet with macros from dict
         for macro in macro_list:
             macro_groups = ",".join (group_map[item] for item in macro['restriction']['ids'])
             ws1.append([macro["title"], macro["id"], macro["created_at"], macro["updated_at"], macro_groups, macro["usage_30d"]])
 
+
+    #print debugging
+    # print(wb.get_sheet_names())
+    del wb['Sheet']
+    # print debugging
+    # print(wb.get_sheet_names())
     wb.save("macro_review.xlsx")
 
     
     pprint('done')
     print(len(macros))
 
-
+# grabs all data from zendesk from url with key to specify what to grab
 def get_macro_list(session, next_url, key="macros"):
     results = []
     while next_url:
